f03bc2b5eb38dfb500822e3adef709d4e041255c
dc <d.chernokur@gmail.com>
2019-10-17 03:29:49 +0300

    Add persistent storage.

--- src/store/index.js
+++ src/store/index.js
@@ -1,14 +1,21 @@
 import Vue from "vue";
 import Vuex from "vuex";
+import VuexPersist from "vuex-persist";
 
 import todo from "@/store/modules/todo";
 
 Vue.use(Vuex);
+
+const vuexPersist = new VuexPersist({
+  key: "todo",
+  storage: window.localStorage
+});
 
 export default new Vuex.Store({
   strict: process.env.NODE_ENV !== "production",
   state: {},
   modules: {
     todo
-  }
+  },
+  plugins: [vuexPersist.plugin]
 });
